3130e02f757aedc8f64b879687f627a2c8c84129
joaquinS25 <mitroncito25@hotmail.com>
2026-05-29 14:47:06 -0500

    BASE DE DATOS ACTUALIZADA

--- BASE DE DATOS/certificados_db.sql
+++ BASE DE DATOS/certificados_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 29-05-2026 a las 02:35:48
+-- Tiempo de generación: 29-05-2026 a las 21:46:05
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.0.30
 
@@ -33,29 +33,30 @@ CREATE TABLE `certificados` (
   `fecha` date DEFAULT NULL,
   `logo_club` varchar(255) DEFAULT NULL,
   `logo_conquistadores` varchar(255) DEFAULT NULL,
-  `fecha_registro` timestamp NOT NULL DEFAULT current_timestamp()
+  `fecha_registro` timestamp NOT NULL DEFAULT current_timestamp(),
+  `clase_biblica` varchar(150) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `certificados`
 --
 
-INSERT INTO `certificados` (`id`, `nombre`, `fecha`, `logo_club`, `logo_conquistadores`, `fecha_registro`) VALUES
-(33, 'AARON KEYLOR CAPUñAY ZAMORA', '2026-05-30', '1000500021.jpg', '1000500020.jpg', '2026-05-28 20:15:14'),
-(35, 'ANDREA BARBARA BELEN VASQUEZ MOREYRA', '2026-05-30', '1000500021.jpg', '1000500020.jpg', '2026-05-28 20:16:50'),
-(36, 'DIEGO FERNANDO SALAS RODRíGUEZ ', '2026-05-30', '1000500021.jpg', '1000500020.jpg', '2026-05-28 20:17:17'),
-(37, 'FABIANA VALENTINA FARFAN CHAVEZ', '2026-05-30', '1000500021.jpg', '1000500020.jpg', '2026-05-28 20:18:28'),
-(38, 'JHON ESNEIDER HERRERA MUñOZ', '2026-05-30', '1000500021.jpg', '1000500020.jpg', '2026-05-28 20:19:15'),
-(39, 'JOSÉ CALED CELIZ GARCIA', '2026-05-30', 'mikel.png', 'conquistadores.png', '2026-05-28 20:20:01'),
-(40, 'LESLIE TATIANA VALDIVIA BUSTAMANTE', '2026-05-30', 'mikel.png', 'conquistadores.png', '2026-05-28 20:20:45'),
-(41, 'LIA FERNANDA CHERAN CAPUÑAY', '2026-05-30', 'mikel.png', 'conquistadores.png', '2026-05-28 20:21:39'),
-(42, 'LIAM MATHIAS ARTEMIO LUYO MOREYRA', '2026-05-30', 'mikel.png', 'conquistadores.png', '2026-05-28 20:22:10'),
-(43, 'LUCIANA ANTONELLA NIÑO CAPUÑAY', '2026-05-30', 'mikel.png', 'conquistadores.png', '2026-05-28 20:22:47'),
-(44, 'LUCIANA VALESKA MARTINEZ MOREYRA		', '2026-05-30', 'mikel.png', 'conquistadores.png', '2026-05-28 20:23:15'),
-(45, 'ROSANGELA VALENTINA HUANCAYO NIÑO', '2026-05-30', 'mikel.png', 'conquistadores.png', '2026-05-28 20:30:07'),
-(46, 'SOFIA EMILY DOMIGUEZ DIACHENKO', '2026-05-30', 'mikel.png', 'conquistadores.png', '2026-05-28 20:30:40'),
-(47, 'THIAGO ALONSO VASQUEZ MOREYRA		', '2026-05-30', 'mikel.png', 'conquistadores.png', '2026-05-28 20:31:06'),
-(48, 'VICTORIA ALEJANDRA DOMINGUEZ DIACHENKO', '2026-05-30', 'mikel.png', 'conquistadores.png', '2026-05-28 20:31:33');
+INSERT INTO `certificados` (`id`, `nombre`, `fecha`, `logo_club`, `logo_conquistadores`, `fecha_registro`, `clase_biblica`) VALUES
+(33, 'AARON KEYLOR CAPUñAY ZAMORA', '2026-05-30', '1000500021.jpg', '1000500020.jpg', '2026-05-28 20:15:14', 'JOYAS DE LA ETERNIDAD'),
+(35, 'ANDREA BARBARA BELEN VASQUEZ MOREYRA', '2026-05-30', '1000500021.jpg', '1000500020.jpg', '2026-05-28 20:16:50', 'SUMERGIDOS EN LA PALABRA'),
+(36, 'DIEGO FERNANDO SALAS RODRíGUEZ ', '2026-05-30', '1000500021.jpg', '1000500020.jpg', '2026-05-28 20:17:17', 'JOYAS DE LA ETERNIDAD'),
+(37, 'FABIANA VALENTINA FARFAN CHAVEZ', '2026-05-30', '1000500021.jpg', '1000500020.jpg', '2026-05-28 20:18:28', 'SUMERGIDOS EN LA PALABRA'),
+(38, 'JHON ESNEIDER HERRERA MUñOZ', '2026-05-30', '1000500021.jpg', '1000500020.jpg', '2026-05-28 20:19:15', 'JOYAS DE LA ETERNIDAD'),
+(39, 'JOSÉ CALED CELIZ GARCIA', '2026-05-30', 'mikel.png', 'conquistadores.png', '2026-05-28 20:20:01', 'SUMERGIDOS EN LA PALBRA'),
+(40, 'LESLIE TATIANA VALDIVIA BUSTAMANTE', '2026-05-30', 'mikel.png', 'conquistadores.png', '2026-05-28 20:20:45', 'SUMERGIDOS EN LA PALABRA'),
+(41, 'LIA FERNANDA CHERAN CAPUÑAY', '2026-05-30', 'mikel.png', 'conquistadores.png', '2026-05-28 20:21:39', 'JOYAS DE LA ETERNIDAD'),
+(42, 'LIAM MATHIAS ARTEMIO LUYO MOREYRA', '2026-05-30', 'mikel.png', 'conquistadores.png', '2026-05-28 20:22:10', 'JOYAS DE LA ETERNIDAD'),
+(43, 'LUCIANA ANTONELLA NIÑO CAPUÑAY', '2026-05-30', 'mikel.png', 'conquistadores.png', '2026-05-28 20:22:47', 'SUMERGIDOS EN LA PALABRA'),
+(44, 'LUCIANA VALESKA MARTINEZ MOREYRA		', '2026-05-30', 'mikel.png', 'conquistadores.png', '2026-05-28 20:23:15', 'JOYAS DE LA ETERNIDAD'),
+(45, 'ROSANGELA VALENTINA HUANCAYO NIÑO', '2026-05-30', 'mikel.png', 'conquistadores.png', '2026-05-28 20:30:07', 'JOYAS DE LA ETERNIDAD'),
+(46, 'SOFIA EMILY DOMIGUEZ DIACHENKO', '2026-05-30', 'mikel.png', 'conquistadores.png', '2026-05-28 20:30:40', 'SUMERGIDOS EN LA PALABRA'),
+(47, 'THIAGO ALONSO VASQUEZ MOREYRA		', '2026-05-30', 'mikel.png', 'conquistadores.png', '2026-05-28 20:31:06', 'JOYAS DE LA ETERNIDAD'),
+(48, 'VICTORIA ALEJANDRA DOMINGUEZ DIACHENKO', '2026-05-30', 'mikel.png', 'conquistadores.png', '2026-05-28 20:31:33', 'JOYAS DE LA ETERNIDAD');
 
 -- --------------------------------------------------------
 
@@ -78,11 +79,11 @@ CREATE TABLE `certificados_especialidades` (
 --
 
 INSERT INTO `certificados_especialidades` (`id`, `nombre`, `especialidad`, `fecha`, `logo_club`, `logo_conquistadores`, `fecha_registro`) VALUES
-(1, 'AARON KEYLOR CAPUÑAY ZAMORA', 'PIONEROS ADVENTISTAS', '2026-04-11', 'mikel.jpg', 'conquistadores.jpg', '2026-05-29 01:15:14'),
-(2, 'ANDREA BARBARA BELEN VASQUEZ MOREYRA', 'PIONEROS ADVENTISTAS', '2026-04-11', 'mikel.jpg', 'conquistadores.jpg', '2026-05-29 01:16:50'),
-(3, 'DIEGO FERNANDO SALAS RODRÍGUEZ', 'PIONEROS ADVENTISTAS', '2026-04-11', 'mikel.jpg', 'conquistadores.jpg', '2026-05-29 01:17:17'),
-(4, 'FABIANA VALENTINA FARFAN CHAVEZ', 'PIONEROS ADVENTISTAS', '2026-04-11', 'mikel.jpg', 'conquistadores.jpg', '2026-05-29 01:18:28'),
-(5, 'JHON ESNEIDER HERRERA MUÑOZ', 'PIONEROS ADVENTISTAS', '2026-04-11', 'mikel.jpg', 'conquistadores.jpg', '2026-05-29 01:19:15'),
+(1, 'AARON KEYLOR CAPUÑAY ZAMORA', 'PIONEROS ADVENTISTAS', '2026-04-11', 'mikel.png', 'conquistadores.png', '2026-05-29 18:56:04'),
+(2, 'ANDREA BARBARA BELEN VASQUEZ MOREYRA', 'PIONEROS ADVENTISTAS', '2026-04-11', 'mikel.png', 'conquistadores.png', '2026-05-29 18:55:49'),
+(3, 'DIEGO FERNANDO SALAS RODRÍGUEZ', 'PIONEROS ADVENTISTAS', '2026-04-11', 'mikel.png', 'conquistadores.png', '2026-05-29 18:55:32'),
+(4, 'FABIANA VALENTINA FARFAN CHAVEZ', 'PIONEROS ADVENTISTAS', '2026-04-11', 'mikel.png', 'conquistadores.png', '2026-05-29 18:55:11'),
+(5, 'JHON ESNEIDER HERRERA MUÑOZ', 'PIONEROS ADVENTISTAS', '2026-04-11', 'mikel.png', 'conquistadores.png', '2026-05-29 18:54:52'),
 (6, 'JOSÉ CALED CELIZ GARCIA', 'PIONEROS ADVENTISTAS', '2026-04-11', 'mikel.png', 'conquistadores.png', '2026-05-29 01:20:01'),
 (7, 'LESLIE TATIANA VALDIVIA BUSTAMANTE', 'PIONEROS ADVENTISTAS', '2026-04-11', 'mikel.png', 'conquistadores.png', '2026-05-29 01:20:45'),
 (8, 'LIA FERNANDA CHERAN CAPUÑAY', 'PIONEROS ADVENTISTAS', '2026-04-11', 'mikel.png', 'conquistadores.png', '2026-05-29 01:21:39'),
@@ -118,7 +119,7 @@ ALTER TABLE `certificados_especialidades`
 -- AUTO_INCREMENT de la tabla `certificados`
 --
 ALTER TABLE `certificados`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
 
 --
 -- AUTO_INCREMENT de la tabla `certificados_especialidades`
